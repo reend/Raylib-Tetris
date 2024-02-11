c79fbb565b268b169988184c7f0d240405f8fced
K.P <prilipko.k@yoju.casino>
2024-02-11 17:34:06 +0100

    core

---
+++ src/game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "grid.h"
+#include "blocks.cpp"
+
+class Game
+{
+    public:
+        Game();
+
+        Block GetRandomBlock();
+        std::vector<Block> GetAllBlocks();
+        void Draw();
+        void HandleInput();
+        void MoveBlockLeft();
+        void MoveBlockRight();
+        void MoveBlockDown();
+        Grid grid;
+        bool gameOver;
+    private:
+        bool isBlockOutside();
+        void RotateBlock();
+        void LockBlock();
+        bool BlockFits();
+        void Reset();
+        std::vector<Block> blocks;
+        Block currentBlock;
+        Block nextBlock;
+};
---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <raylib.h>
+#include "game.h"
+
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    Color darkBlue = {44, 44, 127, 255};
+
+    InitWindow(300, 600, "Tetris");
+    SetTargetFPS(60);
+
+    Game game = Game();
+
+    while(WindowShouldClose() == false)
+    {
+        game.HandleInput();
+        if (EventTriggered(0.2)) 
+        {
+            game.MoveBlockDown();
+        }
+        BeginDrawing();
+        ClearBackground(darkBlue);
+        game.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
